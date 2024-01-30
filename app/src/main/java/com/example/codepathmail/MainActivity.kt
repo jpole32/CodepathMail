4c6c98295132a1ed28740c4f6673b7bd255defda
jpole329 <jpoleary329@gmail.com>
2024-01-30 15:07:05 -0500

    Added load more emails button

--- app/src/main/java/com/example/codepathmail/MainActivity.kt
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.codepathmail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,11 +12,17 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var rViewer = findViewById<RecyclerView>(R.id.emailsRv)
-        var emails = EmailFetcher.getEmails()
+        val rViewer = findViewById<RecyclerView>(R.id.emailsRv)
+        val emails = EmailFetcher.getEmails()
         val adapter = EmailAdapter(emails)
         rViewer.adapter = adapter
         rViewer.layoutManager = LinearLayoutManager(this)
-
+        val loadButton = findViewById<Button>(R.id.submit)
+        loadButton.setOnClickListener {
+            // Fetch next 5 emails and display in RecyclerView
+            val newEmails = EmailFetcher.getNext5Emails()
+            emails.addAll(newEmails)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
